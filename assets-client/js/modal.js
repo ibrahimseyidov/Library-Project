d9e532336f9f4d9dbc45ff330ff88697b7c965bd
ibrahimseyidov <ibrahimseyidov72@gmail.com>
2023-06-14 22:48:53 +0400

    Created Catalog-Product Page

--- assets-client/js/modal.js
+++ assets-client/js/modal.js
@@ -4,21 +4,30 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+let mobileBtn = document.querySelector("#myBtnMobile");
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
+
+mobileBtn.onclick = function () {
+  modal.style.display = "block";
+}
+
+
+
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
