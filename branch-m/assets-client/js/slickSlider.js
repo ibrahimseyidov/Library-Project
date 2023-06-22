da86307cb582a69e2bd1764a073968bfb74339ec
İbrahim <116645047+ibrahimseyidov@users.noreply.github.com>
2023-06-22 14:23:56 +0400

    Delete branch-m directory

--- branch-m/assets-client/js/slickSlider.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $(".your-class").slick({
-    prevArrow: `<div class="prev"><img src="../icons/leftBtn.svg" alt="left-btn"></div>`,
-    nextArrow: `<div class="next"><img src="../icons/rightBtn.svg" alt="right-btn"></div>`,
-  });
-
-})
