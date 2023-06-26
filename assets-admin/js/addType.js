69e0645bb3b950af4a6e0d72e860dbe790480c50
ibrahimseyidov <ibrahimseyidov72@gmail.com>
2023-06-26 18:25:13 +0400

    Search and Other Task

--- assets-admin/js/addType.js
+++ assets-admin/js/addType.js
@@ -6,14 +6,3 @@ $("#xBtn").on("click", function () {
     $(".coment").css("display", "none")
 })
 
-$(".type-btn").on("click", function () {
-    let inpVal = $("#type").val();
-    let option = `<option class="select">${inpVal}</option>`
-    let myArray = JSON.parse(localStorage.getItem("categorie")) || [];
-    myArray.push(inpVal);
-    localStorage.setItem("categorie", JSON.stringify(myArray))
-    $(".form-select").append(option)
-    $("#type").val("");
-})
-
-
