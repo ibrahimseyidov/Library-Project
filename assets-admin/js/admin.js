220490eea6f70a6da49b75847d582b1e431b4e17
ibrahimseyidov <ibrahimseyidov72@gmail.com>
2023-06-28 13:30:49 +0400

    update

--- assets-admin/js/admin.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-    var storedEmail = localStorage.getItem('email');
-    var storedPassword = localStorage.getItem('pass');
-    console.log(storedEmail);
-    console.log(storedPassword);
-    $("#fname").val(storedEmail)
-    $(".pass-inp").val(storedPassword)
-})
--- assets-admin/js/blockHistory.js
+++
@@ -1,7 +0,0 @@
-history.pushState({ page: 'admin.html' }, '', 'panel.html');
-
-window.addEventListener('popstate', function(event) {
-    if (event.state && event.state.page === 'panel.html') {
-        window.location.href = 'panel.html';
-    }
-  });
